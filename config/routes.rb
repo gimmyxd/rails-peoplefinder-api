7bbb79ef6006903f68290522c1cdbcaa7bf6907e
Gimmy <gheorghe.popescu@aserto.com>
2022-06-28 11:14:24 +0300

    init app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1, path: '/' do
+      resources :users
+    end
+  end
+end
